9c2277d73d6fe563e3334a4340a745f936270a16
Paola Quevedo <quevedocontreras.p@husky.neu.edu>
2021-04-19 22:19:51 -0700

    modified env variable

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const app = express();
-let bodyParser = require('body-parser')
+const mongoUri = process.env.MONGODB_URI;
 
 const mongoose = require('mongoose');
 mongoose.connect(
-  'mongodb://localhost:27017/whiteboard', 
-  { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoUri,
+    {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 // CORS Config
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers',
-    'Content-Type, X-Requested-With, Origin');
+      'Content-Type, X-Requested-With, Origin');
   res.header('Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+      'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
 });
 
 app.get('/', (request, response) => {
   response.send('Hello World')
 })
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 const quizzesController = require('./controllers/quizzes-controller')
 quizzesController(app)
